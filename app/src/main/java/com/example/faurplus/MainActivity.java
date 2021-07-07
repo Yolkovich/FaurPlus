e92f8a7f69fd324ab54b27adfa3648ae45ce412e
Blokitin Oleh <blokitin.oleh.pl@gmail.com>
2021-07-07 04:27:20 +0200

    - add third activity "MaxDle" - ImageButton secondScreen connect with activity MaxDle

--- app/src/main/java/com/example/faurplus/MainActivity.java
+++ app/src/main/java/com/example/faurplus/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.ImageButton;
 public class MainActivity extends AppCompatActivity {
 
     ImageButton firstScreen;
+    ImageButton secondScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +18,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ImageButton firstScreen = (ImageButton) findViewById(R.id.imageViewFirstScreen);
+        ImageButton secondScreen = (ImageButton) findViewById(R.id.imageViewSecondScreen);
 
         firstScreen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, Anna.class);
+                startActivity(intent);
+            }
+        });
+
+        secondScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MaxDle.class);
                 startActivity(intent);
             }
         });
