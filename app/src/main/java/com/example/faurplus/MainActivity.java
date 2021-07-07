14f719e7d1d7f7c067339ef5e38dac97eefefd82
Blokitin Oleh <blokitin.oleh.pl@gmail.com>
2021-07-07 04:09:53 +0200

    - add second activity "Anna" - ImageButton firstScreen connect with activity Anna

--- app/src/main/java/com/example/faurplus/MainActivity.java
+++ app/src/main/java/com/example/faurplus/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.faurplus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton firstScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton firstScreen = (ImageButton) findViewById(R.id.imageViewFirstScreen);
+
+        firstScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Anna.class);
+                startActivity(intent);
+            }
+        });
     }
 }
